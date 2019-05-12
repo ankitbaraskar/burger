9451bf94bdc72dccdbebf4487939d5cf055720b0
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-05-12 18:34:00 -0400

    add middleware code to server.js

--- server.js
+++ server.js
@@ -7,7 +7,22 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
 
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
 
 
 app.listen(PORT, function () {

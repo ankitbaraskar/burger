4d390e53183f9e5ce6a28e3adc8668febd00beb0
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-05-12 20:41:04 -0400

    added some sql to update defaults

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,8 @@ use burger_db;
 insert into burgers(burger_name,devoured) values ("Cheese burger",false);
 insert into burgers(burger_name,devoured) values ("Turkey burger",true);
 insert into burgers(burger_name,devoured) values ("Pesto burger",false);
+
+select * from burgers;
+
+update burgers
+set devoured=0 where id in (1,3);

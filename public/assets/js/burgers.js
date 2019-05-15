3e12aa26c26f0913eeb221d29543ffa5ecc129ed
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-05-14 21:43:17 -0400

    added insert new burger from front end js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,29 +1,49 @@
 $(function () {
 
-    $(".devour").on("click",function(event) {
-        let id = $(this).attr("data-id");
-        let burgerName = $(this).attr("data-name");
+  $(".devour").on("click", function (event) {
+    let id = $(this).attr("data-id");
+    let burgerName = $(this).attr("data-name");
 
-        let intID = parseInt(id);
+    let intID = parseInt(id);
 
-        let burgerObject = {
-            id:intID,
-            name:burgerName,
-            devouredTrue:true
-        };
+    let burgerObject = {
+      id: intID,
+      name: burgerName,
+      devouredTrue: true
+    };
 
-        console.log(burgerObject);
+    console.log(burgerObject);
 
-        $.ajax("/api/burgers/" + id, {
-            type: "PUT",
-            data: burgerObject
-          }).then(
-            function() {
-              console.log(`${burgerName} was devoured`);
-              // Reload the page to get the updated list
-              location.reload();
-            }
-          );
-    });
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: burgerObject
+    }).then(
+      function () {
+        console.log(`${burgerName} was devoured`);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-burger").on("click", function (event) {
+    event.preventDefault();
+
+    let burgerName = $("#burger-name-id").val().trim();
+    let burgerObject = {
+      burgerName: burgerName
+    }
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: burgerObject
+    }).then(
+      function () {
+        console.log("created " + burgerObject);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 
 });
